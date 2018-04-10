0da392c5fa02bf527e703e1c75582c588e5e52ad
WeiChien Hsu <weichien711@gmail.com>
2018-04-09 23:05:53 -0500

    :rocket: Add fileAdder file

---
+++ week2/fileAdder.cpp
@@ -0,0 +1,20 @@
+/*********************************************************************
+** Author:       Wei-Chien Hsu
+** Date:         04/09/18
+** Description:  Program reads in the name of the file, and then tries to open it.
+
+                 If the input file is there and can be opened, the program should 
+                 read the list of integers in the file, which will have one integer 
+                 per line.
+
+                 If the input file is not there (or is there but couldn't be opened 
+                 for some reason), the program should just print out "could not 
+                 access file".
+
+                 The program will then add together all the integers in the file, 
+                 create an output file called sum.txt, write the sum to that file 
+                 (just that number - no additional text), and then print (to the 
+                 console) "result written to sum.txt".
+
+                 In the end, close both the input and output files.
+*********************************************************************/
