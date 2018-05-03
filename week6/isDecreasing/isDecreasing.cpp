94299c7c13e27c99678701a6f9e0e981791c7011
WeiChien Hsu <weichien711@gmail.com>
2018-05-02 23:52:21 -0500

    :construcion: Add Recursion Example

---
+++ week6/isDecreasing/isDecreasing.cpp
@@ -0,0 +1,54 @@
+/*********************************************************************
+** Author:       Wei-Chien Hsu
+** Date:         05/02/18
+** Description: a recursive function named isDecreasing that takes as parameters 
+                an array of ints and the size of the array.  It should return true 
+                if the elements of the array are strictly decreasing (each element 
+                in the array is strictly less than the previous one), but return 
+                false otherwise.
+
+** Example    :  {1,2,3,4,5}; -> Return :false
+                 {5,4,3,2,1}; -> Return :true
+                 {5,4,4,3,2}; -> Return :false
+*********************************************************************/
+
+#include <iostream>
+
+/*************************************************************
+ *  Functions prototype
+**************************************************************/
+bool isDecreasing(int arr[], int size);
+
+/*************************************************************
+** isDecreasing(int arr[], int size)
+** Recursive function that will receive the last number in the array,
+** and it will check if that value is strictly larger than the previous
+** one.
+**************************************************************/
+
+
+bool isDecreasing(int arr[], int size) {
+
+    // Out of the arr boundary
+    if(size <= 0) {
+      return true;
+    } else if(arr[size] >= arr[size - 1]) {
+      // Previous value is equal or larger than current value
+      return false;
+    } else {
+      // Current Level don't have any problem
+      // Check the next level
+      return isDecreasing(arr, size - 1);
+    }
+}
+
+int main() {
+  int arr[]  = {1,2,3,4,5};
+  int arr2[] = {5,4,3,2,1};
+  int arr3[] = {5,4,4,3,2};
+  std::cout << isDecreasing(arr, 5) << std::endl;
+  std::cout << isDecreasing(arr2, 5) << std::endl;
+  std::cout << isDecreasing(arr3, 5) << std::endl;
+
+  return 0;
+}
--- week6/vectorPuzzle/vectorPuzzle.cpp
+++ week6/vectorPuzzle/vectorPuzzle.cpp
@@ -1,24 +1,44 @@
 /*********************************************************************
 ** Author:       Wei-Chien Hsu
 ** Date:         05/02/18
-** Description:  
+** Description:  We have a token that starts on the leftmost square.  
+                 On each turn, the token can shift left or right a number 
+                 of squares equal to the value in its current square, 
+                 but is not allowed to move off either end.(Boundry)
 
+                The goal is to get the token to the rightmost square 
+                (that contains zero).  This row has a solution (more than one), 
+                but not all rows do. 
 
+** Example    :  [2, 4, 5, 3, 1, 3, 1, 4, 0] -> Return :true
+                 [1, 3, 2, 1, 3, 4, 0] -> Return :false
+** Remind     : free to use helper methods for any of these projects, meaning 
+                that you can write a function that takes the specified parameters 
+                and have it call your recursive function with any additional 
+                parameters it needs.
 *********************************************************************/
 
 
 #include <iostream>
 #include <vector>
 
+
+/*************************************************************
+ *  Functions prototype
+**************************************************************/
+
+
 bool vectorPuzzle(std::vector<int> &vec, int current, bool visited[]);
 bool vectorPuzzle(std::vector<int> &vec);
 void initializeBoolArray(bool arr[], int size);
 
 
-
 /*************************************************************
 ** initializeBoolArray(bool arr[], int size)
-**
+** This function initialize the Bool Array with fasle values
+** Since the initilize in vectorPuzzle function, we couldn't
+** make sure all values are false, so need an additional method
+** to deal with it.
 **************************************************************/
 
 void initializeBoolArray(bool arr[], int size) {
@@ -29,7 +49,8 @@ void initializeBoolArray(bool arr[], int size) {
 
 /*************************************************************
 ** bool vectorPuzzle(std::vector<int> &vec)
-**
+** This function takes a vector of ints as a parameter and returns true if the 
+** puzzle is solvable for that row, but returns false otherwise.
 **************************************************************/
 
 bool vectorPuzzle(std::vector<int> &vec){
@@ -40,7 +61,17 @@ bool vectorPuzzle(std::vector<int> &vec){
 
 /*************************************************************
 ** bool vectorPuzzle(std::vector<int> &vec, int current, bool visited[])
-**
+** Recursive function:
+** We need to make sure our current index:
+** 1. Not fo out of the boundry : current < 0 or current excesse vector size.
+** 2. Do not visit the place we had already visited : Used a bool array to record
+** 3. Mark the place we just visited : Assign a true value into bool array
+** 4. If the current Value is 0, which means we meet the target, return true
+** Recursion:
+** In each level, we could have two decision going forward or back, so we could
+** use a recursive function to check our next level, if there is a true return
+** value, it means we coudl reach the last number, if we only met the false 
+** situation I've mention above, we will get a all false values reutrn.
 **************************************************************/
 
 bool vectorPuzzle(std::vector<int> &vec, int current, bool visited[]) {
@@ -85,7 +116,6 @@ int main() {
   } else {
     std::cout << "False" << std::endl;
   }
-
 
   return 0;
 }
