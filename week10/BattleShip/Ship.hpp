84a1e06c2f2da54ec188bd5ae3b333620c8b37dd
WeiChien Hsu <weichien711@gmail.com>
2018-06-02 18:43:00 -0500

    :construction: Add Ship and BBoard

--- week10/BattleShip/Ship.hpp
+++ week10/BattleShip/Ship.hpp
@@ -4,5 +4,13 @@
 ** Description:  a class called CFBoard that represents a game board for 
                  playing Connect Four (Links to an external site.)
 
+The Ship class represents a ship that has:
+
+a name (e.g. "my destroyer", "my submarine", "Boaty McBoatface")
+a length (the number of squares it occupies)
+a damage (how many of its squares have been hit)
+a constructor that takes as parameters (in this order): the Ship's name and the Ship's length, which will be used to initialize the corresponding data members.  The damage should be initialized to zero.
+getMethods for each data member (getName, getLength, getDamage)
+a method called takeHit that increments a Ship's damage by one
 
 *********************************************************************/
