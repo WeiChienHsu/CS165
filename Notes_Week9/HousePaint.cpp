69450b1d08932adfebcd45e5c90ee42c835c878a
WeiChien Hsu <weichien711@gmail.com>
2018-06-04 22:48:53 -0500

    :construction: Add enum Example

--- Notes_Week9/HousePaint.cpp
+++ Notes_Week9/HousePaint.cpp
@@ -1,2 +1,30 @@
 #include <iostream>
-#include ""
+#include "HousePaint.hpp"
+
+HousePaint::HousePaint(Color sColor, Color tColor) {
+  setSiding(sColor);
+  setTrim(tColor);
+}
+
+void HousePaint::setSiding(Color sColor) {
+  this->siding = sColor;
+}
+
+Color HousePaint::getSiding() {
+  return this->siding;
+}
+
+void HousePaint::setTrim(Color tColor) {
+  this->trim = tColor;
+}
+
+Color HousePaint::getTrim() {
+  return this->trim;
+}
+
+int main() {
+  HousePaint hp(maroon, ivory);
+  if(hp.getTrim() == ivory) {
+    std::cout << hp.getTrim() << std::endl;
+  }
+}
